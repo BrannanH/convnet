d1eba9ee0b10f3bfcc18bda755d986c7d9219f1a
BrannanH <brannanhancock@gmail.com>
2017-03-26 23:50:21 +0100

    Fixing derivative handling in SeparablePoolingLayer

--- Brannan_AI/src/layers/SeparablePoolingLayer.java
+++ Brannan_AI/src/layers/SeparablePoolingLayer.java
@@ -345,28 +345,21 @@ public abstract class SeparablePoolingLayer implements Layer {
 	private Map<List<Integer>, List<PoolTuple>> updateDerivatives(RecursiveGroup recursiveArguments, List<PoolTuple> result, int poolNumber, int startOfPool, List<Integer> positionInIntermediary) {
 	
 		List<Integer> legacyPosition = arrayAsList(recursiveArguments.position);
-		PoolTuple element = result.get(0);
 		result.remove(0);
 		
-		for(int derivative = 0; derivative < result.size(); derivative++){
-			
-			for(int positionInPool = 0; positionInPool < recursiveArguments.poolSize; positionInPool++) {
+		for(int positionInPool = 0; positionInPool < recursiveArguments.poolSize; positionInPool++) {
 				
-				legacyPosition.set(recursiveArguments.poolingDimension, startOfPool+positionInPool);
+			legacyPosition.set(recursiveArguments.poolingDimension, startOfPool+positionInPool);
 				
-				if(startOfPool+positionInPool == result.get(derivative).origin[recursiveArguments.poolingDimension]){
-					
-					if(recursiveArguments.origins.containsKey(legacyPosition)){
+			if(recursiveArguments.origins.containsKey(legacyPosition)){
 						
-						recursiveArguments.origins.put(positionInIntermediary, recursiveArguments.origins.get(legacyPosition));
-					} else {
-						
-						recursiveArguments.origins.put(positionInIntermediary, result);
-					}
-				} 
-				
+				recursiveArguments.origins.put(positionInIntermediary, recursiveArguments.origins.get(legacyPosition));
 				recursiveArguments.origins.remove(legacyPosition);
+			} else {
+						
+				recursiveArguments.origins.put(positionInIntermediary, result);
 			}
+				
 		}
 		return recursiveArguments.origins;
 	}
